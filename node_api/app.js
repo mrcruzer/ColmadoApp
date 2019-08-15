6a7c8d9d328786eeb863ec97ff37e588564fe47c
Max Sierra <fr3shkid16@gmail.com>
2019-08-15 11:20:20 -0400

    Subir DB en nodejs.

---
+++ node_api/app.js
@@ -0,0 +1,91 @@
+const express = require('express')
+const mongoose = require("mongoose")
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = 2020
+app.use(bodyParser.json())
+// Base de datos NoSQL
+const url = "mongodb+srv://maxsierra16:mxfresh.16@cluster0-nzbfd.mongodb.net/test?retryWrites=true&w=majority"
+mongoose.connect(url, { useNewUrlParser: true, useFindAndModify: false }) // 
+
+const ClienteSchema = new mongoose.Schema({
+    nombre: String,
+    limite_credito: Number,
+    deuda: { type: Number, default: 0 }
+});
+const Cliente = mongoose.model("Cliente", ClienteSchema);
+
+// Endponits
+app.get('/', (req, res)=>{
+    try{
+        res.render("form.ejs")
+    } catch(error){
+        console.error(error)
+    }
+})
+
+app.post('/fiao', async (req, res)=>{
+    try{
+        const {nombre, limite_credito} = req.body
+        const data = {
+            nombre,
+            limite_credito
+        }
+        console.log(req)
+        // await Cliente.create(data, (error, nuevoCliente)=>{
+        //     if(error) throw new Error(error)
+        //     res.send(nuevoCliente)
+        // })
+    } catch(error){
+        res.status(500).send(error)
+    }
+})
+
+app.get('/fiao', async (req, res)=>{
+    await Cliente.find({}, (error, data)=>{
+        if(error) throw new Error(error)
+        res.render('show.ejs', {data})
+    })
+})
+
+app.get('/fiao/:id', async (req, res)=>{
+    const id = req.params.id
+    await Cliente.findById(id, (error, cliente)=>{
+        if(error || !cliente){ res.redirect('/fiao') }
+        res.send(cliente)
+    })
+})
+
+app.put('/fiao/:id', async (req, res)=>{
+    try {
+        const id = req.params.id
+        const newInfo = req.body
+        
+        await Cliente.findByIdAndUpdate(id, newInfo, (error, cliente)=>{
+            if(error || !cliente) { res.send("Id es incorrecto") }
+            res.send(cliente)
+        })
+        
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+app.delete('/fiao/:id', async (req, res)=>{
+    try {
+        const id = req.params.id
+        if(!id){ res.send('id es incorrecto') }
+
+        await Cliente.findByIdAndRemove(id, (error, cliente)=>{
+            if(error) { res.redirect("/fiao"+id) }
+            res.send(`Cliente fue borrado!`)
+        })
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+app.listen(port, ()=>{
+    console.log(`Corriendo en puerto ${port}`)
+})
